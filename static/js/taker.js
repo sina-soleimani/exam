4fd0385b9c4a6163c46fc17052d417d7c7832907
sina <sinasoleimahijee@gmail.com>
2023-09-18 10:31:32 -0700

    add to do to taker

--- static/js/taker.js
+++ static/js/taker.js
@@ -1,10 +1,8 @@
 
+//TODO
 $(document).on('click', 'button.showqbtn', function (event) {
 
-    console.log( $(this).parent().parent().children('.q-desc')[0].innerHTML.trim())
-
-
-
+    console.log($(this).parent().parent().children('.q-desc')[0].innerHTML.trim())
 
     $('#qlistModal').modal('toggle');
 })
