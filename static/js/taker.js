d88e4d58659fd5f1d3689208c78ab098e05d75ce
sina <sinasoleimahijee@gmail.com>
2023-09-24 12:06:39 -0700

    add dataset and work with this in taker

--- static/js/taker.js
+++ static/js/taker.js
@@ -1,8 +1,26 @@
-
 //TODO
 $(document).on('click', 'button.showqbtn', function (event) {
+    const question = findQuestionById(questionGroupsData, $(this).data('id'));
 
-    console.log($(this).parent().parent().children('.q-desc')[0].innerHTML.trim())
+    var description = question.description.replaceAll('\r\n', '<br>');
+    $("#descriptionContent").html(description)
 
     $('#qlistModal').modal('toggle');
 })
+
+function findQuestionById(questionGroupsData, questionId) {
+    for (let i = 0; i < questionGroupsData.length; i++) {
+        const group = questionGroupsData[i];
+
+        for (let j = 0; j < group.questions.length; j++) {
+            const question = group.questions[j];
+
+            if (question.id === questionId) {
+                return question;
+            }
+        }
+    }
+
+    // If the question is not found, return null
+    return null;
+}
