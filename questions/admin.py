ead4f2606b31b2d4658d93a0b82728bed152fb7f
sina <sinasoleimahijee@gmail.com>
2023-10-27 10:18:27 -0700

    questiona admin updated

--- questions/admin.py
+++ questions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Question, QuestionTrueFalse,QuestionGroup, Answer
+from .models import Question, QuestionTrueFalse,QuestionGroup, Answer, Choice
 # Register your models here.
 
 admin.site.register(Question)
 admin.site.register(QuestionTrueFalse)
 admin.site.register(QuestionGroup)
 admin.site.register(Answer)
+admin.site.register(Choice)
