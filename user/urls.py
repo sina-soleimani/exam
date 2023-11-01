2567978f9ee59ca7bcbc7bb2241a59d566a762e2
sina <sinasoleimahijee@gmail.com>
2023-11-01 01:38:23 -0700

    user logout added

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'user'
+
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import ProfileCreationForm, ProfileLoginForm
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
 
 
 def register(request):
@@ -9,7 +11,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect('/login/')
     else:
         form = ProfileCreationForm()
     return render(request, 'registration/register.html', {'form': form})
@@ -25,3 +27,6 @@ def user_login(request):
     else:
         form = ProfileLoginForm()
     return render(request, 'registration/login.html', {'form': form})
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('user:login')
