0444b89a5efdb27299b6e030cff72939536fe3b9
sina <sinasoleimahijee@gmail.com>
2023-12-08 09:25:22 -0700

    result url updated

--- result/urls.py
+++ result/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ResultListView, calc_result
+from .views import ResultListView, calc_result, ResultDetailListView
 
 app_name = 'results'
 
 urlpatterns = [
     path('list/<int:id>', ResultListView.as_view(), name='resultList'),
+    path('details/<int:id>', ResultDetailListView.as_view(), name='resultDetails'),
     path('calc_result/', calc_result, name='calc_result'),
 
 ]
