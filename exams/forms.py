dd40b043b1ec498fa2e6bb3afb982621f101eafb
sina <sinasoleimahijee@gmail.com>
2024-09-14 17:15:08 +0330

    form of exam app ready for release

--- exams/forms.py
+++ exams/forms.py
@@ -5,18 +5,17 @@ from .models import Exam
 class ExamForm(forms.ModelForm):
     class Meta:
         model = Exam
-        fields = ['label', 'duration',
+        fields = ['label', 'duration', 'exam_date', 'exam_time',
                   'score_type',
                   'point_passing_score', 'percent_passing_score',
                   'incorrect_penalty', 'unanswered_penalty', 'shuffle_answer', 'manual_chosen'
                   ]
 
 
-
 class ExamFormUpdate(forms.ModelForm):
     class Meta:
         model = Exam
-        fields = ['label', 'duration', 'score_type',
+        fields = ['label', 'duration', 'score_type', 'exam_date', 'exam_time',
                   'point_passing_score', 'percent_passing_score',
                   'incorrect_penalty', 'unanswered_penalty', 'shuffle_answer', 'manual_chosen'
                   ]
