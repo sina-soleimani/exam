abd7d42ea343a3505196a78e28e85865e97b1209
sina <sinasoleimahijee@gmail.com>
2024-09-14 18:26:35 +0330

    urls of home app ready for release

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    path('exam/index', views.exam_index, name='exam_home'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
