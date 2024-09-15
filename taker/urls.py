b4ad1271b2469433887a61df23c8c9e440fe7612
sina <sinasoleimahijee@gmail.com>
2024-09-15 16:23:00 +0330

    url and view taker app ready for release

--- taker/urls.py
+++ taker/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import examSession,AnswerQuestionView
+from .views import examSession,AnswerQuestionView, ExamDoneView
 
 app_name = 'taker'
 
 urlpatterns = [
     path('<str:id>/exam_session', examSession.as_view(), name='examSession'),
     path('answer_question/<int:id>', AnswerQuestionView.as_view(), name='answer_question'),
+    path('exam_done/<int:id>', ExamDoneView.as_view(), name='exam_done'),
 ]
--- taker/views.py
+++ taker/views.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pytz
 from django.forms import model_to_dict
 from django.http import Http404
 from django.shortcuts import render
@@ -13,6 +15,8 @@ import json
 from django.contrib.auth.decorators import login_required
 from decorator import access_level_required
 from user.models import STUDENT_ACCESS, ADMIN_ACCESS
+import jdatetime
+from django.db import transaction
 
 
 class LoginRequiredMixin:
@@ -26,7 +30,43 @@ class examSession(LoginRequiredMixin, View):
     @access_level_required(STUDENT_ACCESS)
     def get(self, request, id):
         exam = Exam.objects.get(id=id)
-        if exam.exam_status != 'A':
+        x = jdatetime.date.fromgregorian(date=datetime.now())
+        iran_tz = pytz.timezone('Asia/Tehran')
+
+        # Get the current time in Iran
+        current_time = datetime.now(iran_tz)
+
+        # Format the time as a string
+        current_time_str = current_time.strftime('%H:%M:%S')
+
+        print("Current time in Iran:", current_time_str)
+
+        y = x.strftime('%m/%d/%Y')
+        print(y[4:])
+        print(y[:5])
+        z = y[4:] + '/' + y[:5]
+        print(z)
+        print(type(exam.exam_time))
+        print(type(exam.exam_time.strftime('%H:%M:%S')))
+        print(type(exam.duration))
+        print(type(str(exam.duration)))
+
+        print((datetime.combine(datetime.date(current_time), exam.exam_time) + exam.duration).time())
+        current_date = datetime.now().date()
+
+        # Convert time objects to datetime objects with the same date
+        # datetime1 = datetime.combine(current_date, time1)
+        datetime2 = datetime.combine(current_date, exam.exam_time)
+        timezone = pytz.timezone('Asia/Tehran')
+        datetime2 = timezone.localize(datetime2)
+        print('current_time < exam.exam_time + exam.duration')
+        # print(current_time < exam.exam_time + exam.duration)
+        if exam.exam_date == z[2:] and current_time_str > exam.exam_time.strftime(
+                '%H:%M:%S') and exam.duration > current_time - datetime2:
+            sina = 2
+        else:
+
+            # print('yyyyyyyyyyyyy')
             raise Http404("Exam status is not 'ACTIVE'")
 
         questions = list(exam.questions.all())
@@ -93,8 +133,8 @@ class examSession(LoginRequiredMixin, View):
         json_data = json.dumps(q)
 
         result = Result.objects.get_or_create(exam_id=id, student_id=request.user.id)
-        current_time = datetime.now()
-        time_difference = current_time - exam.active_time
+        # current_time = datetime.now()
+        time_difference = current_time - datetime2
         remain_time = exam.duration - time_difference
         s_remain_time = remain_time.total_seconds()
         if s_remain_time < 0:
@@ -106,6 +146,7 @@ class examSession(LoginRequiredMixin, View):
             'questions': questions,
             'remain_time': s_remain_time,
             'result_id': result[0].id,
+            'result_score': result[0].score,
             'exam_id': exam.id,
         })
 
@@ -164,25 +205,85 @@ class AnswerQuestionView(LoginRequiredMixin, CreateView):
                 choice = models.Choice.objects.get(id=self.request.POST.get('mc_id'))
                 profile_answer.choice = choice
             elif self.request.POST.get('q_type') == 'MG':
-                matches_objects = []
+                with transaction.atomic():
+                    matches_objects = []
 
-                for match_id, item_id, item_text, match_text in zip(match_ids, item_ids, item_texts, match_texts):
-                    match = ProfMatch.objects.create(
-                        match_text=match_text,
-                        match_id=match_id,
-                        item_text=item_text,
-                        item_id=item_id
-                    )
+                    for match_id, item_id, item_text, match_text in zip(match_ids, item_ids, item_texts, match_texts):
+                        match = ProfMatch.objects.create(
+                            match_text=match_text,
+                            match_id=match_id,
+                            item_text=item_text,
+                            item_id=item_id
+                        )
 
-                    matches_objects.append(match)
-                profile_answer.save()
+                        matches_objects.append(match)
+                    profile_answer.save()
 
-                profile_answer.match.add(*matches_objects)
+                    profile_answer.match.add(*matches_objects)
 
             profile_answer.result = result
             profile = Profile.objects.get(id=user_id)
             profile_answer.student = profile
             profile_answer.exam = Exam.objects.get(id=exam_id)
             profile_answer.save()
+
+        return super().form_valid(form)
+
+
+class ExamDoneView(LoginRequiredMixin, CreateView):
+    model = ProfileAnswer
+    template_name = 'home/taker.html'
+    success_url = '/success/'
+    form_class = None
+
+    def get_form_class(self):
+
+        return ProfileAnswerForm
+
+    @access_level_required(STUDENT_ACCESS)
+    def form_valid(self, form):
+        user_id = self.request.user.id
+        exam_id = self.request.POST.get('exam_id')
+        result = Result.objects.get(id=self.request.POST.get('result_id'))
+        exam_sum_score = 0
+        result_number = 0
+        profile_answer = form.instance
+
+        score = 0
+
+        for result_prof_answer in result.result_prof_answers.all():
+            # print(result_prof_answer.question.question_type)
+
+            for question_choice in result_prof_answer.question.question_choices.filter(is_true=True):
+                if question_choice.id == result_prof_answer.choice.id:
+                    score += result_prof_answer.question.score
+            all_match = 0
+            correct_match = 0
+
+            for question_match in result_prof_answer.match.all():
+                all_match = all_match + 1
+                if question_match.item_id == question_match.match_id:
+                    correct_match = correct_match + 1
+
+            if all_match != 0:
+                score += int((correct_match / all_match) * result_prof_answer.question.score)
+
+            for question_answer in result_prof_answer.question.question_answers.all():
+                print(f"Answer avl: {question_answer.is_true}")
+
+                if question_answer.is_true == result_prof_answer.is_true:
+                    score += result_prof_answer.question.score
+        if score != 0:
+            exam_sum_score = exam_sum_score + score
+            result_number = result_number + 1
+
+        result.score = score
+        result.save()
+
+        profile_answer.result = result
+        profile = Profile.objects.get(id=user_id)
+        profile_answer.student = profile
+        profile_answer.exam = Exam.objects.get(id=exam_id)
+        profile_answer.save()
 
         return super().form_valid(form)
